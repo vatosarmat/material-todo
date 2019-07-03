6953358323fb0cecee614b1f9a6cc673269d4716
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-03 05:12:14 +0000

    Deploy vatosarmat/material-todo to github.com/vatosarmat/material-todo.git:gh-pages

---
+++ precache-manifest.6764c36c8f517093c998fcf60324ce8e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f5b8c1fee14f32aa90b6128a0560231",
+    "url": "/material-todo/index.html"
+  },
+  {
+    "revision": "d0c46cede1c78ad509f6",
+    "url": "/material-todo/static/js/2.75c3a129.chunk.js"
+  },
+  {
+    "revision": "7bb4f4149aa1913a3f5d",
+    "url": "/material-todo/static/js/main.92cb38f0.chunk.js"
+  },
+  {
+    "revision": "c2fd9124530668cc4cd4",
+    "url": "/material-todo/static/js/runtime~main.20498bfa.js"
+  }
+]);
